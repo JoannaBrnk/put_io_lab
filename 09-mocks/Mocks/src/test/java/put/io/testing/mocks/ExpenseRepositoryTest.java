6d52f11d320458b9f5bee11436550f678efaed71
Asia <joanna.brnk@gmail.com>
2020-12-14 11:23:00 +0100

    add tests

---
+++ 09-mocks/Mocks/src/test/java/put/io/testing/mocks/ExpenseRepositoryTest.java
@@ -0,0 +1,16 @@
+package put.io.testing.mocks;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.*;
+import org.mockito.*;
+
+import put.io.students.fancylibrary.database.FancyDatabase;
+import put.io.students.fancylibrary.database.IFancyDatabase;
+
+import java.util.Collections;
+
+public class ExpenseRepositoryTest {
+
+}
